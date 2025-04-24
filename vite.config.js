74862d8d1bc9280f222c96231ce50d35bdf2bc51
Feras Saleem <ferassaleem83@gmail.com>
2025-04-24 12:38:11 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import ghPages from 'vite-plugin-gh-pages'
 
-// اكتب اسم الريبو بدل "your-repo-name"
-const repoName = 'ads'
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: `/${repoName}/`, // مهم عشان GitHub Pages
+  plugins: [react()],
+  base: `/ads/`, // مهم عشان GitHub Pages
 })
